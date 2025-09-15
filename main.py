e5767522c1fc446f27661c2d48482ef5945e966c
zya.willis <zya@willisio.com>
2025-09-15 18:33:08 +1000

    added "main.py"

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 def main():
-    """
-    This is the main function of the bookbot 'main.py'
-    """
+    
+# Define variables
+
     file_path = "/home/zya/projects/boot.dev/bookbot/books/frankenstein.txt"
+    word = ""
+
+# This is the main function of the bookbot 'main.py'
+    
     get_book_text(file_path)
+
     prnt_file_contents = get_book_text(file_path)
+
     print(prnt_file_contents)
+
+# Define functions
 
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
     return file_contents
 
+# Return the number of words in a string
+
+def num_words(file_path):
+    
 main()
