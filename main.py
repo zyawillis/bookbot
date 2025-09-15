18f9d909c497aef4fceb35b1f494b675657ee367
zya.willis <zya@willisio.com>
2025-09-15 21:32:30 +1000

    Progress made to "num_words" function in "main.py"

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ def main():
 # Define variables
 
     file_path = "/home/zya/projects/boot.dev/bookbot/books/frankenstein.txt"
-    word = ""
 
 # This is the main function of the bookbot 'main.py'
     
@@ -11,7 +10,15 @@ def main():
 
     prnt_file_contents = get_book_text(file_path)
 
-    print(prnt_file_contents)
+### Print whole book to terminal ###
+
+    # print(prnt_file_contents)
+
+    prnt_num_words = num_words(prnt_file_contents)
+
+### Print each word to terminal ###
+
+    print(prnt_num_words)
 
 # Define functions
 
@@ -22,6 +29,9 @@ def get_book_text(file_path):
 
 # Return the number of words in a string
 
-def num_words(file_path):
-    
+def num_words(prnt_file_contents):
+    """ May need to take out the f statement and tac on later. Need to count the words before output to console """
+    split_words = f"{prnt_file_contents.split(" ")} words found in the document"
+    return split_words
+
 main()
